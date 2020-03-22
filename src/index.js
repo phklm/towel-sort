85c0931e230a0bd5a358bdeaba01e7925226e3e4
phkl <phkl@tuta.io>
2020-03-22 20:19:49 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length == 0) return [];
+  let result = [];
+    matrix.forEach((a, b) => {
+      if (b % 2 != 0) a.reverse();
+      result = result.concat(a);
+    });
+  return result;
 }
